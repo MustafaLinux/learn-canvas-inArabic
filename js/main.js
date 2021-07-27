b6a487c52db51eb57f4a736663c11d7468fa2cb5
mustafa-oil <74876263+mustafa-oil@users.noreply.github.com>
2021-07-27 10:18:29 +0300

    basic website design

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const links = document.querySelectorAll('.links');
+const sections = document.querySelectorAll('section');
+
+function changeLinkState() {
+  let index = sections.length;
+
+  while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+  
+  links.forEach((link) => link.classList.remove('active'));
+  links[index].classList.add('active');
+}
+
+changeLinkState();
+window.addEventListener('scroll', changeLinkState);
